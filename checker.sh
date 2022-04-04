2466aced92b30f61c4ab86d109c0f5465fffa4e8
6r6 <r6r@live.cn>
2022-04-04 13:09:22 +0800

    update checker.sh and readme.md

--- checker.sh
+++ checker.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 # 检查叮咚买菜是否有可配送时段,有则通过Bark推送
 
+# (*)请填充BarkId
+barkId=""
+
 while :; do
 
 echo "正在检查是否有可用配送时段..."
 
-# replace your curl command here
+# (*)请填充cURL命令,别忘记输出到tmp.json
 # curl https://maicai.api.ddxq.mobi/order/getMultiReserveTime > tmp.json
 
-availableCount=`cat tmp.json | jq -r '.data[0].time[0].times[].disableType' | grep -c 0`
+responseCheckCount=`cat tmp.json | grep -c 'station_id'`
+
+if [[ $responseCheckCount -ne 1 ]]; then
+    echo "😭抱歉 请检查cURL命令是否能获取到正确的数据"
+    exit 1
+fi
+
+# api version: 9.49.0
+availableCount=`cat tmp.json | jq -r '.data[0]?.time[0].times[].disableType' | grep -c 0`
+
+# api version: 9.24.3
+availableCount=`cat tmp.json | jq -r '.data.time[0].times[].disableType' | grep -c 0`
+
 if [[ $availableCount -gt 0 ]]; then
     echo "🎉恭喜 发现可用的配送时段 请尽快下单!"
-    curl "https://api.day.app/<your-bark-id>/叮咚买菜有可用配送时段请尽快下单/"
+    curl "https://api.day.app/$barkId/叮咚买菜有可用配送时段请尽快下单/"
     exit 0
 fi
 
-echo "无可用配送时段 休眠10秒再试..."
-sleep 10
+echo "无可用配送时段 休眠15秒再试..."
+sleep 15
 
 done
--- readme.md
+++ readme.md
@@ -1,16 +1,23 @@
-## maicai.ddxq.tools
+## maicai.ddxq.tools  
 
-### Utils
+### Utils  
 | Name  | Description  |
 | ------------ | ------------ |
 |  checker.sh | 检查今天是否有可配送时段，有则推送到手机  |
 
+### Versions  
+| Name  | Api Version  |
+| ------------ | ------------ |
+|  unionpay 微信小程序 | 9.23.0  |
+|  bcm 交通银行 | 9.24.3  |
+|  weixin 微信小程序 | 9.49.0  |
+|  alipay 支付宝小程序 | 9.49.0  |
 
-### Usage
+### Usage  
 
 ```bash
 # checker.sh
-wget https://raw.staticdn.net/6r6/maicai.ddxq.tools/main/checker.sh 
+wget https://raw.staticdn.net/6r6/maicai.ddxq.tools/main/checker.sh
 # 修改curl指令和bark-id然后保存
 bash checker.sh
 ```
